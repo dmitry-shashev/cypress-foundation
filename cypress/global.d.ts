dba9752a4b479ce28aef2c9ebb13a85c33f89513
Dimon <old.reaver@gmail.com>
2023-07-10 10:53:35 +0300

    Some table func

--- cypress/global.d.ts
+++ cypress/global.d.ts
@@ -2,6 +2,24 @@ declare global {
   namespace Cypress {
     interface Chainable<Subject> {
       clickIfExists(element: string): Chainable<Subject>
+      checkTableTextContent(
+        selector: string,
+        row: number,
+        column: number,
+        value: string
+      ): Chainable<Subject>
+      checkTableImgAltContent(
+        selector: string,
+        row: number,
+        column: number,
+        value: string
+      ): Chainable<Subject>
+      checkTableImgSrcContent(
+        selector: string,
+        row: number,
+        column: number,
+        value: string
+      ): Chainable<Subject>
     }
   }
 }
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -13,3 +13,41 @@ Cypress.Commands.add('clickIfExists', (content: string) => {
       }
     })
 })
+
+Cypress.Commands.add(
+  'checkTableTextContent',
+  (selector: string, row: number, column: number, value: string) => {
+    cy.get(selector)
+      .find('tbody tr')
+      .eq(row)
+      .find('td')
+      .eq(column)
+      .contains(value, { matchCase: false })
+  }
+)
+
+Cypress.Commands.add(
+  'checkTableImgAltContent',
+  (selector: string, row: number, column: number, value: string) => {
+    cy.get(selector)
+      .find('tbody tr')
+      .eq(row)
+      .find('td')
+      .eq(column)
+      .find(`img[alt="${value}"]`)
+      .should('exist')
+  }
+)
+
+Cypress.Commands.add(
+  'checkTableImgSrcContent',
+  (selector: string, row: number, column: number, value: string) => {
+    cy.get(selector)
+      .find('tbody tr')
+      .eq(row)
+      .find('td')
+      .eq(column)
+      .find(`img[src*="${value}"]`)
+      .should('exist')
+  }
+)
